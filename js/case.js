0fca03e2babf516558aae4d3c980dec2cc726a1e
mharif1 <md5667676@gmail.com>
2022-08-19 01:58:57 +0600

    formeted

--- js/case.js
+++ js/case.js
@@ -11,11 +11,11 @@ function updateCaseNumber(isIncrease) {
         newCaseNumber = previousCaseNumber - 1;
     }
     caseNumberField.value = newCaseNumber;
-   
+
     return newCaseNumber;
 }
 
-function updateCaseTotalNumber(newCaseNumber){
+function updateCaseTotalNumber(newCaseNumber) {
     const caseTotalPrice = newCaseNumber * 59;
     const caseTotalElement = document.getElementById('case-total');
     caseTotalElement.innerText = caseTotalPrice;
@@ -28,7 +28,7 @@ document.getElementById('btn-case-plus').addEventListener('click', function () {
     // const newCaseNumber = previousCaseNumber + 1;
     // caseNumberField.value =newCaseNumber ;
     // console.log(previousCaseNumber)
-    
+
     const newCaseNumber = updateCaseNumber(true);
 
     // const caseTotalPrice = newCaseNumber * 59;
@@ -42,9 +42,9 @@ document.getElementById('btn-case-minus').addEventListener('click', function () 
     // const previousCaseNumber = parseInt(caseNumberString);
     // const newCaseNumber =previousCaseNumber - 1;
     // caseNumberField.value =newCaseNumber;
-    
+
     const newCaseNumber = updateCaseNumber(false);
-    
+
     // const caseTotalPrice = newCaseNumber * 59;
     // const caseTotalElement = document.getElementById('case-total');
     // caseTotalElement.innerText = caseTotalPrice;
