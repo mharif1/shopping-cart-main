384d855c805f5509d47df40864269b446610b079
mharif1 <md5667676@gmail.com>
2022-08-19 03:01:18 +0600

    function failed

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,26 @@
+// function updatePhoneNumber (isIncrease){
+//     const phoneNumberField = document.getElementById('phone-number-field');
+//     const phoneNumberString = phoneNumberField.value;
+//     const previousPhoneNumber = parseInt(phoneNumberString);
+
+//     let newPhoneNumber;
+//     if(isIncrease === true){
+//         const newPhoneNumber = previousPhoneNumber + 1;
+//     }
+//     else{
+//         const newPhoneNumber = previousPhoneNumber - 1;
+//     }
+//     phoneNumberField.value = newPhoneNumber ;
+    
+//     return newPhoneNumber;
+// }
+
+// function updatePhoneTotalNumber(newPhoneNumber){
+//     const phoneTotalPrice = newPhoneNumber * 1219;
+//     const phoneTotalElement = document.getElementById('phone-total');
+//     phoneTotalElement.innerText = phoneTotalPrice ;
+// }
+
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
     const phoneNumberField = document.getElementById('phone-number-field');
     const phoneNumberString = phoneNumberField.value;
@@ -9,6 +32,9 @@ document.getElementById('btn-phone-plus').addEventListener('click', function(){
     const phoneTotalPrice = newPhoneNumber * 1219;
     const phoneTotalElement = document.getElementById('phone-total');
     phoneTotalElement.innerText = phoneTotalPrice ;
+   
+    // const newPhoneNumber = updatePhoneNumber(true);
+    // updatePhoneTotalNumber(newPhoneNumber)
 })
 
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
@@ -22,4 +48,7 @@ document.getElementById('btn-phone-minus').addEventListener('click', function(){
     const phoneTotalPrice = newPhoneNumber * 1219;
     const phoneTotalElement = document.getElementById('phone-total');
     phoneTotalElement.innerText = phoneTotalPrice ;
+
+    // const newPhoneNumber = updatePhoneNumber(false);
+    // updatePhoneTotalNumber(newPhoneNumber)
 })
